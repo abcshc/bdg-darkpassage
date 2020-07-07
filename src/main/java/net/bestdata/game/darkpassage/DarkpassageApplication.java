0cb950df11cdcdbbbe81a7dbfb05f243c818566b
Heechul So <thrrkthrrk@gmail.com>
2020-07-07 13:09:37 +0900

    Initialize

---
+++ src/main/java/net/bestdata/game/darkpassage/DarkpassageApplication.java
@@ -0,0 +1,13 @@
+package net.bestdata.game.darkpassage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkpassageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkpassageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/bestdata/game/darkpassage/DarkpassageApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bestdata.game.darkpassage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkpassageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
