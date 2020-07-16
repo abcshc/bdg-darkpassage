cb6a68a49c1ba1b4cbd01ec31ac76b9b2a5bc145
Heechul So <thrrkthrrk@gmail.com>
2020-07-16 17:21:44 +0900

    Revert "Change sending RiotRequest to RabbitMQ"
    This reverts commit 32d7e75a

--- src/main/java/net/bestdata/game/darkpassage/services/SummonerService.java
+++ src/main/java/net/bestdata/game/darkpassage/services/SummonerService.java
@@ -1,10 +1,10 @@
 package net.bestdata.game.darkpassage.services;
 
 import net.bestdata.game.darkpassage.model.enums.Region;
+import net.bestdata.game.darkpassage.repositories.RiotRequestRepository;
 import net.bestdata.game.darkpassage.repositories.SummonerRepository;
 import net.bestdata.game.darkpassage.repositories.documents.RiotRequest;
 import net.bestdata.game.darkpassage.repositories.documents.Summoner;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -13,25 +13,20 @@ import java.time.Duration;
 @Service
 public class SummonerService {
     private final SummonerRepository summonerRepository;
-    private final RabbitTemplate rabbitTemplate;
+    private final RiotRequestRepository riotRequestRepository;
 
-    public SummonerService(SummonerRepository summonerRepository, RabbitTemplate rabbitTemplate) {
+    public SummonerService(SummonerRepository summonerRepository, RiotRequestRepository riotRequestRepository) {
         this.summonerRepository = summonerRepository;
-        this.rabbitTemplate = rabbitTemplate;
+        this.riotRequestRepository = riotRequestRepository;
     }
 
     public Mono<Summoner> find(String summonerName, Region region) {
         return summonerRepository.findBySummonerNameAndRegion(summonerName, region)
-                .switchIfEmpty(
-                        summonerRepository.findBySummonerNameAndRegion(summonerName, region)
-                                .doFirst(() -> rabbitTemplate.convertAndSend(RiotRequest.newSummonerRequest(summonerName, region)))
-                                .delaySubscription(Duration.ofSeconds(3))
-                );
+                .switchIfEmpty(riotRequestRepository.save(RiotRequest.newSummonerRequest(summonerName, region))
+                        .then(summonerRepository.findBySummonerNameAndRegion(summonerName, region).delaySubscription(Duration.ofSeconds(3))));
     }
 
     public Mono<RiotRequest> refresh(String summonerName, Region region) {
-        RiotRequest riotRequest = RiotRequest.newSummonerRequest(summonerName, region);
-        rabbitTemplate.convertAndSend(riotRequest);
-        return Mono.just(riotRequest);
+        return riotRequestRepository.save(RiotRequest.newSummonerDetailRequest(summonerName, region));
     }
 }
